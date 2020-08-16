fe5d3ad0a65950ad9fe052192bc6795c11f92ee3
Thakur-Govind <govind.thakur11@gmail.com>
2020-08-16 13:34:43 +0530

    work so far

--- cfg24/cfg/views.py
+++ cfg24/cfg/views.py
@@ -16,63 +16,44 @@ from django.urls import reverse_lazy
 
 '''Registration,login,logout start'''
 
-
-
-def register(request):
-    if request.method == 'POST':
-        form2 = FarmerForm(request.POST)
-        form=AccountAuthenticationForm(request.POST)
-        print("1",form.is_valid())
-        print("2",form2.is_valid())
-
-        if form2.is_valid():
-            print("-------->",form2.data)
-            if ("is_farmer" in form2.data.keys()):
-                print("------------->","yes")
-                Farmer=form2.save()
-                Farmer.set_password(Farmer.password)
-                Farmer.is_farmer = True
-                Farmer.is_buyer=False
-                Farmer.save()
-
-                return redirect('signup')
-            else:
-                print("no")
-                print("Its a buyer")
-                form3=BuyerForm(data=form2.data)
-                print(form3)
-                Buyer=form3.save()
-
-                # buyer=Buyer.objects.create(form2.data)
-                Buyer.set_password(Buyer.password)
-                Buyer.is_farmer = False
-                Buyer.is_buyer=True
-                Buyer.save()
-                return redirect('signup')
-            # Farmer=form.save()
-            # Farmer.set_password(Farmer.password)
-            # Farmer.is_farmer = True
-            # Farmer.is_buyer=False
-            # Farmer.save()
-            # return redirect('login')
-
-        elif form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(username=username, password=password)
-            if user:
-                login(request, user)
-                if user.is_farmer:
-                    return redirect('farmer_home')
-                else:
-                    return redirect('product_list')
-    else:
-        form=AccountAuthenticationForm()
-        form2 = FarmerForm()
-
-    return render(request, 'FarmerApp/login.html', {'form2': form2,'form':form})
-
+def login_student(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user:
+        login(request, user)
+        student = Student.objects.get(name = username)
+        return render(request,'StudentDashboard.html',{'student':student})
 
 def logout_view(request):
 	logout(request)
 	return redirect('signup')
+
+
+
+def student_home(request):
+    student = request.user
+
+    return render(request, 'StudentDashboard.html',{'student':student})
+
+def teacher_home(request):
+    teacher = request.user
+    return render(request, 'StudentDashboard.html',{'teacher':teacher})
+
+def add_course(request,course_id):
+    course=get_object_or_404(Course,pk=course_id)
+    student = Student.objects.get(name=request.user.username)#TBC
+    student_courses = json.loads(student.courses)
+    student_courses[len(student_courses)+1] = {'course':course,'complete':False}
+    student.courses = student_courses
+    student.save()
+    return render(request,'StudentDashboard.html') #message to be added later
+
+def view_courses(request):
+    courses = Courses.objects.all()
+    return render(request,'Courses.html',{'courses':courses})
+
+def register_course(request):
+    new_course = Courses()
+    new_course.name = request.POST['coursename']
+    #request.
